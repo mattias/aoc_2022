05926dbbe55377dafadebab5dd4292afc9880fe7
Mattias Lövkvist <mattiaslovkvist@gmail.com>
2022-12-01 20:07:12 +0100

    Day 1 solution

--- day01/aoc.go
+++ day01/aoc.go
@@ -3,39 +3,71 @@ package main
 import (
 	"fmt"
 	"os"
+	"sort"
 	"strconv"
 	"strings"
 )
 
-func getSolutionPart1(input []int) int {
-	sum := 0
-	for _, value := range input {
-		sum += value
+func calculateCaloriesPerElf(input [][]int) *[]int {
+	caloriesPerElf := make([]int, 0)
+	var sum int
+
+	for _, calorieGroup := range input {
+		sum = 0
+		for _, calorie := range calorieGroup {
+			sum += calorie
+		}
+
+		caloriesPerElf = append(caloriesPerElf, sum)
 	}
-	return sum
+
+	return &caloriesPerElf
 }
 
-func getSolutionPart2(input []int) int {
-	product := 1
-	for _, value := range input {
-		product *= value
-	}
-	return product
+func sortCaloriesByDescending(caloriesPerElf *[]int) {
+	sort.Sort(sort.Reverse(sort.IntSlice(*caloriesPerElf)))
 }
 
-func parseInput(input string) ([]int, error) {
-	var ints []int
+func top3Highest(caloriesPerElf *[]int) int {
+	return (*caloriesPerElf)[0] + (*caloriesPerElf)[1] + (*caloriesPerElf)[2]
+}
+
+func getSolutionPart1(input [][]int) int {
+	caloriesPerElf := calculateCaloriesPerElf(input)
+	sortCaloriesByDescending(caloriesPerElf)
+
+	return (*caloriesPerElf)[0]
+}
+
+func getSolutionPart2(input [][]int) int {
+	caloriesPerElf := calculateCaloriesPerElf(input)
+	sortCaloriesByDescending(caloriesPerElf)
+
+	return top3Highest(caloriesPerElf)
+}
+
+func parseInput(input string) ([][]int, error) {
+	var ints [][]int
 
 	lines := strings.Split(strings.TrimSpace(input), "\r\n")
+	j := 0
+	temp := make([]int, 0)
 
 	for _, line := range lines {
+		if line == "" {
+			j++
+			ints = append(ints, [][]int{temp}...)
+			temp = make([]int, 0)
+			continue
+		}
 		i, err := strconv.Atoi(line)
 		if err != nil {
 			return nil, err
 		}
-
-		ints = append(ints, i)
+		temp = append(temp, i)
 	}
+
+	ints = append(ints, [][]int{temp}...)
 
 	return ints, nil
 }
--- day01/aoc_test.go
+++ day01/aoc_test.go
@@ -7,8 +7,8 @@ import (
 )
 
 func TestAOC_parseInput(t *testing.T) {
-	input := "1337\r\n42"
-	expectedParsedInput := []int{1337, 42}
+	input := "1000\r\n2000\r\n3000\r\n\r\n4000\r\n\r\n5000\r\n6000\r\n\r\n7000\r\n8000\r\n9000\r\n\r\n10000"
+	expectedParsedInput := [][]int{{1000, 2000, 3000}, {4000}, {5000, 6000}, {7000, 8000, 9000}, {10000}}
 
 	actualParsedInput, err := parseInput(input)
 	assert.NoError(t, err)
@@ -16,16 +16,16 @@ func TestAOC_parseInput(t *testing.T) {
 }
 
 func TestAOC_getSolutionPart1(t *testing.T) {
-	input := []int{1337, 42}
-	expectedSolution := 1337 + 42
+	input := [][]int{{1000, 2000, 3000}, {4000}, {5000, 6000}, {7000, 8000, 9000}, {10000}}
+	expectedSolution := 24000
 
 	actualSolution := getSolutionPart1(input)
 	assert.Equal(t, expectedSolution, actualSolution)
 }
 
 func TestAOC_getSolutionPart2(t *testing.T) {
-	input := []int{1337, 42}
-	expectedSolution := 1337 * 42
+	input := [][]int{{1000, 2000, 3000}, {4000}, {5000, 6000}, {7000, 8000, 9000}, {10000}}
+	expectedSolution := 45000
 
 	actualSolution := getSolutionPart2(input)
 	assert.Equal(t, expectedSolution, actualSolution)
