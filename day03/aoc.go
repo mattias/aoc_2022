c29adaa2b1db1f66fe7b2a810bcb6113d1244038
Mattias Lövkvist <mattiaslovkvist@gmail.com>
2022-12-03 13:24:20 +0100

    part 1 day 3

--- day03/aoc.go
+++ day03/aoc.go
@@ -4,10 +4,81 @@ import (
 	"fmt"
 	"os"
 	"strings"
+	"unicode"
 )
 
+type rucksack struct {
+	firstCompartment  string
+	secondCompartment string
+}
+
+func newRucksuck(rawRucksack string) *rucksack {
+	r := rucksack{}
+	halfRucksack := (len(rawRucksack) / 2)
+	r.firstCompartment = rawRucksack[:halfRucksack]
+	r.secondCompartment = rawRucksack[halfRucksack:]
+	return &r
+}
+
+type rucksackOrganizer struct {
+	priorities map[rune]int
+	rucksacks  []rucksack
+}
+
+func newRucksackOrganizer() *rucksackOrganizer {
+	ro := rucksackOrganizer{}
+	ro.priorities = make(map[rune]int)
+	ro.rucksacks = make([]rucksack, 0)
+
+	lowerCounter := 1
+	upperCounter := 27
+	for r := 'a'; r <= 'z'; r++ {
+		ro.priorities[r] = lowerCounter
+		lowerCounter++
+		ro.priorities[unicode.ToUpper(r)] = upperCounter
+		upperCounter++
+	}
+
+	return &ro
+}
+
+func (ro *rucksackOrganizer) addRucksacks(rucksacks []string) {
+	for _, rawRucksack := range rucksacks {
+		r := newRucksuck(rawRucksack)
+		ro.rucksacks = append(ro.rucksacks, *r)
+	}
+}
+
+func (ro *rucksackOrganizer) priorityOfItemAppearingInBothCompartmentsOfEachRucksack() []int {
+	var priorities []int
+
+	for _, r := range ro.rucksacks {
+		for _, firstType := range r.firstCompartment {
+			for _, secondType := range r.secondCompartment {
+				if firstType == secondType {
+					points := ro.priorities[rune(firstType)]
+					priorities = append(priorities, points)
+					goto nextRucksack
+				}
+			}
+		}
+	nextRucksack:
+	}
+
+	return priorities
+}
+
 func getSolutionPart1(input []string) int {
-	return len(input)
+	ro := newRucksackOrganizer()
+	ro.addRucksacks(input)
+
+	sum := 0
+
+	for _, priority := range ro.priorityOfItemAppearingInBothCompartmentsOfEachRucksack() {
+		sum += priority
+	}
+
+	return sum
 }
 
 func getSolutionPart2(input []string) int {
