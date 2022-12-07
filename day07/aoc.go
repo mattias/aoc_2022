4c49fa27d01124a9e3f3bc2a23f7bc9e4abf15cc
Mattias Lövkvist <mattiaslovkvist@gmail.com>
2022-12-07 23:12:21 +0100

    day 7

--- day07/aoc.go
+++ day07/aoc.go
@@ -3,41 +3,180 @@ package main
 import (
 	"fmt"
 	"os"
+	"sort"
 	"strconv"
 	"strings"
 )
 
-func getSolutionPart1(input []int) int {
-	sum := 0
-	for _, value := range input {
-		sum += value
-	}
-	return sum
+type file struct {
+	name string
+	size int
 }
 
-func getSolutionPart2(input []int) int {
-	product := 1
-	for _, value := range input {
-		product *= value
-	}
-	return product
+func newFile(name string) *file {
+	file := file{}
+	file.name = name
+
+	return &file
 }
 
-func parseInput(input string) ([]int, error) {
-	var ints []int
+type directory struct {
+	parentDirectory *directory
+	name            string
+	directories     []*directory
+	files           []*file
+}
+
+func newDirectory(parentDirectory *directory, name string) *directory {
+	dir := directory{}
+	dir.parentDirectory = parentDirectory
+	dir.name = name
+
+	return &dir
+}
+
+func (dir *directory) isRoot() bool {
+	return dir.parentDirectory == nil
+}
+
+func (dir *directory) size() int {
+	size := 0
+
+	for _, file := range dir.files {
+		size += file.size
+	}
+
+	for _, directory := range dir.directories {
+		size += directory.size()
+	}
+
+	return size
+}
+
+type filesystem struct {
+	root directory
+	cwd  *directory
+}
+
+func newFilesystem() *filesystem {
+	root := directory{}
+	root.name = "/"
+
+	fs := filesystem{}
+	fs.root = root
+	fs.cwd = &root
+	return &fs
+}
+
+func (fs *filesystem) cd(command string) {
+	if command == ".." {
+		if fs.cwd.isRoot() {
+			return
+		}
+
+		fs.cwd = fs.cwd.parentDirectory
+	}
+
+	if command == "/" {
+		fs.cwd = &fs.root
+	}
+
+	for _, dir := range fs.cwd.directories {
+		if strings.TrimSpace(dir.name) == strings.TrimSpace(command) {
+			fs.cwd = dir
+			break
+		}
+	}
+}
+
+func (fs *filesystem) addDir(name string) {
+	fs.cwd.directories = append(fs.cwd.directories, newDirectory(fs.cwd, strings.TrimSpace(name)))
+}
+
+func (fs *filesystem) addFile(size int, name string) {
+	newFile := newFile(strings.TrimSpace(name))
+	newFile.size = size
+	fs.cwd.files = append(fs.cwd.files, newFile)
+}
+
+func (dir *directory) directorySizeArray() []int {
+	sizes := []int{}
+
+	for _, subDir := range dir.directories {
+		sizes = append(sizes, subDir.directorySizeArray()...)
+	}
+
+	sizes = append(sizes, dir.size())
+	return sizes
+}
+
+func getSolutionPart1(fs *filesystem) int {
+	directorySizes := fs.root.directorySizeArray()
+
+	totalSum := 0
+	for _, size := range directorySizes {
+		if size < 100000 {
+			totalSum += size
+		}
+	}
+
+	return totalSum
+}
+
+func getSolutionPart2(fs *filesystem) int {
+	unusedSpace := 70000000 - fs.root.size()
+	spaceToGet := 30000000 - unusedSpace
+
+	directorySizes := fs.root.directorySizeArray()
+
+	sort.Ints(directorySizes)
+
+	for _, size := range directorySizes {
+		if size > spaceToGet {
+			return size
+		}
+	}
+
+	return 0
+}
+
+func stringToInt(input string) (int, error) {
+	i, err := strconv.Atoi(input)
+	if err != nil {
+		return 0, err
+	}
+
+	return i, nil
+}
+
+func parseInput(input string) *filesystem {
+	fs := newFilesystem()
 
 	lines := strings.Split(strings.TrimSpace(input), "\r\n")
 
 	for _, line := range lines {
-		i, err := strconv.Atoi(line)
-		if err != nil {
-			return nil, err
+		if line[0] == '$' {
+			if line[2:4] == "cd" {
+				fs.cd(line[5:])
+				continue
+			}
+
+			if line[2:4] == "ls" {
+				continue
+			}
 		}
 
-		ints = append(ints, i)
+		if line[:3] == "dir" {
+			fs.addDir(line[4:])
+			continue
+		}
+
+		fileSplit := strings.Split(line, " ")
+		size, _ := stringToInt(fileSplit[0])
+		fs.addFile(size, fileSplit[1])
 	}
 
-	return ints, nil
+	return fs
 }
 
 func main() {
@@ -46,10 +185,7 @@ func main() {
 		panic("couldn't read input")
 	}
 
-	input, err := parseInput(string(inputBytes))
-	if err != nil {
-		panic("couldn't parse input")
-	}
+	input := parseInput(string(inputBytes))
 
 	fmt.Println("Go")
 	part := os.Getenv("part")
--- day07/aoc_test.go
+++ day07/aoc_test.go
@@ -6,26 +6,17 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestAOC_parseInput(t *testing.T) {
-	input := "1337\r\n42"
-	expectedParsedInput := []int{1337, 42}
-
-	actualParsedInput, err := parseInput(input)
-	assert.NoError(t, err)
-	assert.Equal(t, expectedParsedInput, actualParsedInput)
-}
-
 func TestAOC_getSolutionPart1(t *testing.T) {
-	input := []int{1337, 42}
-	expectedSolution := 1337 + 42
+	input := parseInput("$ cd /\r\n$ ls\r\ndir a\r\n14848514 b.txt\r\n8504156 c.dat\r\ndir d\r\n$ cd a\r\n$ ls\r\ndir e\r\n29116 f\r\n2557 g\r\n62596 h.lst\r\n$ cd e\r\n$ ls\r\n584 i\r\n$ cd ..\r\n$ cd ..\r\n$ cd d\r\n$ ls\r\n4060174 j\r\n8033020 d.log\r\n5626152 d.ext\r\n7214296 k")
+	expectedSolution := 95437
 
 	actualSolution := getSolutionPart1(input)
 	assert.Equal(t, expectedSolution, actualSolution)
 }
 
 func TestAOC_getSolutionPart2(t *testing.T) {
-	input := []int{1337, 42}
-	expectedSolution := 1337 * 42
+	input := parseInput("$ cd /\r\n$ ls\r\ndir a\r\n14848514 b.txt\r\n8504156 c.dat\r\ndir d\r\n$ cd a\r\n$ ls\r\ndir e\r\n29116 f\r\n2557 g\r\n62596 h.lst\r\n$ cd e\r\n$ ls\r\n584 i\r\n$ cd ..\r\n$ cd ..\r\n$ cd d\r\n$ ls\r\n4060174 j\r\n8033020 d.log\r\n5626152 d.ext\r\n7214296 k")
+	expectedSolution := 24933642
 
 	actualSolution := getSolutionPart2(input)
 	assert.Equal(t, expectedSolution, actualSolution)
